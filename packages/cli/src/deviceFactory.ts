9c3aed549e7b99b0d08315d938b2ee8896d61f83
Felipe Jaramillo <Felipe.jaramillo@dedalus.com>
2024-05-26 20:53:26 +0200

    fix: rename Rpi2In7V2

--- packages/cli/src/deviceFactory.ts
+++ packages/cli/src/deviceFactory.ts
@@ -60,7 +60,7 @@ async function getRpi7in5V2(orientation?: Orientation, colorMode?: ColorMode): P
 
 async function getRpi2In7(orientation?: Orientation, colorMode?: ColorMode): Promise<DisplayDevice> {
     try {
-        const { Rpi2In7: Rpi2In7V2 } = await import('@epaperjs/rpi-2in7');
+        const { Rpi2In7 } = await import('@epaperjs/rpi-2in7');
         return new Rpi2In7(orientation, colorMode);
     } catch (e) {
         throw new Error('Failed to import @epaperjs/rpi-2in7, make sure it is installed');
@@ -69,7 +69,7 @@ async function getRpi2In7(orientation?: Orientation, colorMode?: ColorMode): Pro
 
 async function getRpi2In7V2(orientation?: Orientation, colorMode?: ColorMode): Promise<DisplayDevice> {
     try {
-        const { Rpi2In7: Rpi2In7V2 } = await import('@epaperjs/rpi-2in7-v2');
+        const { Rpi2In7V2 } = await import('@epaperjs/rpi-2in7-v2');
         return new Rpi2In7V2(orientation, colorMode);
     } catch (e) {
         throw new Error('Failed to import @epaperjs/rpi-2in7, make sure it is installed');
--- packages/rpi-2in7-v2/src/rpi2In7V2.ts
+++ packages/rpi-2in7-v2/src/rpi2In7V2.ts
@@ -3,7 +3,7 @@ import { ImageOptions } from '@epaperjs/core/src/image/imageOptions';
 import bindings from 'bindings';
 import { Driver } from './driver';
 
-export class Rpi2In7 implements DisplayDevice {
+export class Rpi2In7V2 implements DisplayDevice {
     public readonly height: number;
     public readonly width: number;
     private readonly driver: Driver;
